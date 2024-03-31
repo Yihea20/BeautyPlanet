f975bfbf5107b612cf40e4bda22d41b2edd9b6f4
Yihea20 <111204473+Yihea20@users.noreply.github.com>
2024-04-01 00:23:46 +0300

    change to person

--- BeautyPlanet/Services/AuthoManger.cs
+++ BeautyPlanet/Services/AuthoManger.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace BeautyPlanet.Services
 {
