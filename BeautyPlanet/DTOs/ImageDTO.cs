ed07c1d9e14df002145514dc3903822aae3c6534
yiheayihea95@gmail.com <yiheayihea95@gmail.com>
2024-04-04 14:57:25 +0300

    dto

---
+++ BeautyPlanet/DTOs/ImageDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace BeautyPlanet.DTOs
+{
+    public class CreateImage
+    {
+
+        public string Name { get; set; }
+        public int GalleryId { get; set; }
+
+    }
+    public class ImageDTO : CreateImage
+    {
+        public int Id { get; set; }
+        public string? URL { get; set; }
+    }
+    public class ImageFile
+    {
+        public CreateImage Create { get; set; }
+        public IFormFile file { get; set; }
+
+    }
+}
--- BeautyPlanet/DTOs/UserLoginDTO.cs
+++ BeautyPlanet/DTOs/UserLoginDTO.cs
@@ -2,8 +2,17 @@
 
 namespace BeautyPlanet.DTOs
 {
+    public class UserRegistDTO : UserLoginDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<string> RoleName { get; set; }
+        public int CenterId { get; set; }
+
+    }
     public class UserLoginDTO
     {
+
         public string Email { get; set; }
         public string Password { get; set; }
         [DataType(DataType.PhoneNumber)]
