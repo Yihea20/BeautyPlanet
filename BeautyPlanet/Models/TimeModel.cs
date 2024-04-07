f9b253d6678bda20bfc57b308515be6b8d3bc8c3
yiheayihea95@gmail.com <yiheayihea95@gmail.com>
2024-04-07 14:48:09 +0300

    Time

---
+++ BeautyPlanet/Models/TimeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BeautyPlanet.Models
+{
+    public class TimeModel
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public ICollection<Specialist> Specialists { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
